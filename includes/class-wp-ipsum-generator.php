a6e0ad9b7782befda9f2a25ee170cec674ca0431
Pascal Birchler <hello@pascalbirchler.ch>
2014-08-13 18:40:11 +0200

    Initial commit

---
+++ includes/class-wp-ipsum-generator.php
@@ -0,0 +1,276 @@
+<?php
+
+/**
+ * Class SpinPress_WP_Ipsum_Generator.
+ *
+ * Inspired by the Bacon Ipsum generator.
+ *
+ * @see https://github.com/petenelson/bacon-ipsum
+ *
+ */
+class SpinPress_WP_Ipsum_Generator {
+
+	/**
+	 * @var array
+	 */
+	protected $options = array();
+
+	/**
+	 * Class Constructor.
+	 *
+	 * @param mixed $args An array or string containing the arguments.
+	 */
+	public function __construct( $args ) {
+
+		$defaults = array(
+			'use_filler_words' => false,
+			'paragraphs' => 5,
+			'start_with_lorem' => true,
+		);
+
+		$this->options = wp_parse_args( $args, $defaults );
+	}
+
+	protected function get_words() {
+
+		$wpwords = array(
+			// Release Names
+			'WordPress',
+			'Jazz',
+			'Davis',
+			'Blakey',
+			'Mingus',
+			'Strayhorn',
+			'Duke',
+			'Boren',
+			'Ella',
+			'Getz',
+			'Dexter',
+			'Brecker',
+			'Tyner',
+			'Coltrane',
+			'Baker',
+			'Carmen',
+			'Monk',
+			'Reinhardt',
+			'Gershwin',
+			'Stitt',
+			'Green',
+			'Jones',
+			'Oscar',
+			'Basie',
+			'Parker',
+			'Smith',
+
+			// People
+			'Matt',
+			'Mullenweg',
+			'Mark',
+			'Jaquith',
+			'Ozz',
+			'Andrew',
+			'Nacin',
+			'Otto',
+			'Westi',
+			'Helen',
+			'Jen',
+			'Andrea',
+			'Donncha',
+			'Mike',
+
+			// Companies etc.
+			'Automattic',
+			'Foundation',
+			'WordCamp',
+			'Meetup',
+
+			// Components
+			'Post',
+			'Theme',
+			'Plugin',
+			'Filter',
+			'Action',
+			'Multisite',
+			'Transient',
+
+			// General
+			'Release',
+			'Update',
+			'Upgrade',
+			'Language',
+			'Install',
+			'Chat',
+			'Premium',
+			'Haiku',
+			'GPL',
+			'b2',
+			'blog',
+			'website',
+
+			// Other projects
+			'Akismet',
+			'BackPress',
+			'BuddyPress',
+			'bbPress',
+			'VaultPress',
+		);
+
+		$filler = array(
+			'consectetur',
+			'adipisicing',
+			'elit',
+			'sed',
+			'do',
+			'eiusmod',
+			'tempor',
+			'incididunt',
+			'ut',
+			'labore',
+			'et',
+			'dolore',
+			'magna',
+			'aliqua',
+			'ut',
+			'enim',
+			'ad',
+			'minim',
+			'veniam',
+			'quis',
+			'nostrud',
+			'exercitation',
+			'ullamco',
+			'laboris',
+			'nisi',
+			'ut',
+			'aliquip',
+			'ex',
+			'ea',
+			'commodo',
+			'consequat',
+			'duis',
+			'aute',
+			'irure',
+			'dolor',
+			'in',
+			'reprehenderit',
+			'in',
+			'voluptate',
+			'velit',
+			'esse',
+			'cillum',
+			'dolore',
+			'eu',
+			'fugiat',
+			'nulla',
+			'pariatur',
+			'excepteur',
+			'sint',
+			'occaecat',
+			'cupidatat',
+			'non',
+			'proident',
+			'sunt',
+			'in',
+			'culpa',
+			'qui',
+			'officia',
+			'deserunt',
+			'mollit',
+			'anim',
+			'id',
+			'est',
+			'laborum');
+
+
+		if ( true === $this->options['use_filler_words'] ) {
+			$words = array_merge( $wpwords, $filler );
+		} else {
+			$words = $wpwords;
+		}
+
+		// Everyday I'm shufflin'
+		shuffle( $words );
+
+		return $words;
+
+	}
+
+	protected function generate_sentence() {
+		// A sentence should be between 4 and 15 words.
+		$sentence = '';
+		$length = rand( 4, 15 );
+
+		// Add a little more randomness to commas, about 2/3rds of the time
+		$include_comma = $length >= 7 && rand( 0, 2 ) > 0;
+
+		$words = $this->get_words();
+
+		if ( count( $words ) > 0 ) {
+
+			for ( $i = 0; $i < $length; $i++ ) {
+
+				if ( $i > 0 ) {
+
+					if ( $i >= 3 && $i != $length - 1 && $include_comma ) {
+
+						if (rand(0,1) == 1) {
+							$sentence = rtrim( $sentence ) . ', ';
+							$include_comma = false;
+						} else {
+							$sentence .= ' ';
+						}
+
+					} else {
+
+						$sentence .= ' ';
+
+					}
+
+				}
+
+				$sentence .= $words[$i];
+			}
+
+			$sentence = rtrim($sentence) . '. ';
+		}
+
+		// Capitalize the first word, rest is lowercase
+		$sentence =  ucfirst( strtolower( $sentence ) );
+
+		return $sentence;
+
+	}
+
+	protected function generate_paragraph()	{
+		// A paragraph should be between 4 and 7 sentences.
+
+		$paragraph = '';
+		$length = rand( 4, 7 );
+
+		for ($i = 0; $i < $length; $i++) {
+			$paragraph .= $this->generate_sentence() . ' ';
+		}
+
+		return rtrim( $paragraph );
+
+	}
+
+	public function generate_lorem_ipsum() {
+
+		$paragraphs = array();
+
+		for ( $i = 0; $i < $this->options['paragraphs']; $i++ ) {
+			$p = $this->generate_paragraph();
+
+			if ( 0 === $i && $this->options['start_with_lorem'] ) {
+				$p = 'WordPress ipsum dolor sit amet ' . strtolower( $p );
+			}
+
+			$paragraphs[]  = ucfirst( $p );
+		}
+
+		return $paragraphs;
+
+	}
+
+}
---
+++ wp-ipsum.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * WordPress Ipsum Generator
+ *
+ * @package   WP_Ipsum
+ * @author    Pascal Birchler <pascal.birchler@spinpress.com>
+ * @license   GPL-2.0+
+ * @link      http://spinpress.com
+ * @copyright 2014 SpinPress
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WordPress Ipsum
+ * Plugin URI:        http://spinpress.com
+ * Description:       Bored of all the normal lorem ipsum texts? Here's a lorem ipsum generator full of WordPress. Isn't that awesome?
+ * Version:           1.0.0
+ * Author:            Pascal Birchler
+ * Author URI:        http://spinpress.com
+ * Text Domain:       wp-ipsum
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+add_shortcode('wp-ipsum', 'spinpress_wp_ipsum_form');
+
+function spinpress_wp_ipsum_form($atts) {
+	$output = '';
+
+	global $wp;
+	$current_url = add_query_arg( $wp->query_string, '', home_url( $wp->request ) );
+
+	if ( isset( $_REQUEST['filler'] ) ) {
+		$use_filler_words = (bool) $_REQUEST['filler'];
+	} else {
+		$use_filler_words = false;
+	}
+
+	if ( isset( $_REQUEST['lorem'] ) ) {
+		$start_with_lorem = (bool) $_REQUEST['lorem'];
+	} else if ( isset ( $_REQUEST['paragraphs'] ) ) {
+		$start_with_lorem = false;
+	} else {
+		$start_with_lorem = true;
+	}
+
+	$num_paragraphs = 5;
+
+	if ( isset( $_REQUEST['paragraphs'] ) ) {
+		$num_paragraphs = absint( $_REQUEST['paragraphs'] );
+
+		if ( 50 < $num_paragraphs ) {
+			$num_paragraphs = 50;
+		}
+	}
+
+	if ( isset( $_REQUEST['paragraphs'] ) ) {
+
+		require_once( plugin_dir_path( __FILE__ ) . 'includes/class-wp-ipsum-generator.php' );
+
+		$args = array(
+			'paragraphs'       => $num_paragraphs,
+			'use_filler_words' => $use_filler_words,
+			'start_with_lorem' => $start_with_lorem
+		);
+
+		$generator = new SpinPress_WP_Ipsum_Generator( $args );
+
+		$paragraphs = $generator->generate_lorem_ipsum();
+
+		$output .= '<div class="wp-ipsum-text">';
+		foreach( $paragraphs as $p ) {
+			$output .= '<p>' . $p . '</p>';
+		}
+
+		$output .= '</div>';
+	}
+
+	$form = '
+		<form class="wp-ipsum-form" action="' . esc_url( $current_url ) . '" method="get">
+			<p>
+				<label class="wp-ipsum-label wp-ipsum-label-paragraphs" for="wp-ipsum-paragraphs"> ' . __( 'Paragraphs', 'wp-ipsum' ) . '</label>
+				<input class="wp-ipsum-input" type="number" id="wp-ipsum-paragraphs" name="paragraphs" min="1" max="50" value="' . $num_paragraphs . '" maxlength="2" />
+			</p>
+			<p>
+				<label class="wp-ipsum-label"><input class="wp-ipsum-input" type="radio" name="filler" value="0" ' . checked( $use_filler_words, false, false ) . ' />All WordPress</label>
+				<label class="wp-ipsum-label"><input class="wp-ipsum-input" type="radio" name="filler" value="1" ' . checked( $use_filler_words, true, false ) . ' />WordPress and Filler</label>
+			</p>
+			<p>
+				<input class="wp-ipsum-input wp-ipsum-input-start" type="checkbox" name="lorem" id="wp-ipsum-start_with_lorem" value="1" ' . checked( $start_with_lorem, true, false ) . '  />
+				<label class="wp-ipsum-label wp-ipsum-label-start" for="wp-ipsum-start_with_lorem">' . __( 'Start with “WordPress ipsum dolor sit amet...”', 'wp-ipsum' ) . '</label>
+			</p>
+			<p>
+				<input class="wp-ipsum-input" type="submit" value="' . __( 'Generate', 'wp-ipsum' ) . '" />
+			</p>
+		</form>
+	';
+
+	return $form . $output;
+
+}
